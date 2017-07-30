68574e6b6d9c1acf299ed106ed24cb4495867d98
Ondrej Krajcik <o.krajcik@seznam.cz>
2017-07-30 11:27:14 +0200

    TokenResult: downgrade for php < 7, add expiratedAt DateTime

--- src/Result/TokenResult.php
+++ src/Result/TokenResult.php
@@ -2,6 +2,8 @@
 
 namespace Autopoctive\ApiClient\Result;
 
+use Nette\DateTime;
+
 class TokenResult {
 
 	/** @var  string */
@@ -10,63 +12,62 @@ class TokenResult {
 	/** @var  int */
 	protected $expiresIn;
 
+	/** @var  \DateTime */
+	protected $expiresAt;
+
 	/** @var  string */
 	protected $refreshToken;
 
 
 	/**
 	 * TokenResult constructor.
-	 * @param string $accessToken
-	 * @param int    $expiresIn
-	 * @param string $refreshToken
+	 * @param string    $accessToken
+	 * @param int       $expiresIn
+	 * @param string    $refreshToken
+	 * @param \DateTime $expiresAt
 	 */
-	public function __construct($accessToken, $expiresIn, $refreshToken) {
+	public function __construct($accessToken, $expiresIn, $refreshToken, \DateTime $expiresAt) {
 		$this->accessToken = $accessToken;
 		$this->expiresIn = $expiresIn;
 		$this->refreshToken = $refreshToken;
+		$this->expiresAt = $expiresAt;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function getAccessToken(): string {
+	public function getAccessToken() {
 		return $this->accessToken;
-	}
-
-	/**
-	 * @param string $accessToken
-	 */
-	public function setAccessToken(string $accessToken) {
-		$this->accessToken = $accessToken;
 	}
 
 	/**
 	 * @return int
 	 */
-	public function getExpiresIn(): int {
+	public function getExpiresIn() {
 		return $this->expiresIn;
-	}
-
-	/**
-	 * @param int $expiresIn
-	 */
-	public function setExpiresIn(int $expiresIn) {
-		$this->expiresIn = $expiresIn;
 	}
 
 	/**
 	 * @return string
 	 */
-	public function getRefreshToken(): string {
+	public function getRefreshToken() {
 		return $this->refreshToken;
 	}
 
 	/**
-	 * @param string $refreshToken
+	 * @return \DateTime
 	 */
-	public function setRefreshToken(string $refreshToken) {
-		$this->refreshToken = $refreshToken;
+	public function getExpiresAt() {
+		return $this->expiresAt;
 	}
 
+	/**
+	 * Is token valid?
+	 * @return bool
+	 */
+	public function isValid() {
+		$now = new DateTime();
+		return $now <  $this->expiresAt;
+	}
 
 }
